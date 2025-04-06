58d4c8130aa23204840073e31f9c4ffc98c43b19
DiedCloud <fkirill2604@gmail.com>
2025-04-06 06:34:58 +0300

    feat: init миграция

---
+++ src/main/resources/db/migration/V0_0_1__INIT.sql
@@ -0,0 +1,34 @@
+create table User
+(
+    id       bigserial primary key,
+    login    varchar(255) not null unique,
+    name     varchar(255) not null,
+    password varchar(255) not null
+);
+
+create table Comment
+(
+    id        bigserial primary key,
+    author_id bigint    not null,
+    content   text      not null,
+    published timestamp not null,
+    foreign key (user_id) REFERENCES User (id) ON DELETE cascade ON UPDATE cascade
+);
+
+create table Video_Info
+(
+    id          bigserial primary key,
+    title       varchar(511) not null,
+    description text         not null,
+    published   timestamp    not null,
+    author_id   bigint       not null,
+    foreign key (user_id) REFERENCES User (id) ON DELETE cascade ON UPDATE cascade
+);
+
+create table Monetization_Info
+(
+    video_id  bigint primary key,
+    percent   float not null,
+    is_agreed bool  not null,
+    foreign key (video_id) REFERENCES Video_Info (id) ON DELETE cascade ON UPDATE cascade
+);
